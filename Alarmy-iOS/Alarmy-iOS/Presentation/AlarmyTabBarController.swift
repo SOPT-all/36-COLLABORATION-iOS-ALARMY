1c0855e0d1b9a587b53343dd5529cdbdb117e320
eunji <oieunxe@gmail.com>
2025-05-12 23:13:56 +0900

    refactor: 코드 구조 개선

--- Alarmy-iOS/Alarmy-iOS/Presentation/AlarmyTabBarController.swift
+++ Alarmy-iOS/Alarmy-iOS/Presentation/AlarmyTabBarController.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 class AlarmyTabBarController: UITabBarController, UITabBarControllerDelegate {
+    
+    private enum TabIndex: Int {
+        case alarm = 0
+        case sleep
+        case morning
+        case report
+        case setting
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,79 +25,74 @@ class AlarmyTabBarController: UITabBarController, UITabBarControllerDelegate {
         setupTabBarAppearance()
     }
 
-    func configureController() {
+    private func configureController() {
         let alarmViewController = tabBarNavigationController(
-            unselectedImage: symbolImage(name: "icon_gnb_clockoff"),
-            selectedImage: symbolImage(name: "icon_gnb_clockon"),
+            unselectedImage: assetImage(name: "icon_gnb_clockoff"),
+            selectedImage: assetImage(name: "icon_gnb_clockon"),
             rootViewController: AlarmViewController(),
             title: "알람"
         )
 
-        let sleepViewController = UIViewController()
-        sleepViewController.tabBarItem = UITabBarItem(
-            title: "수면",
-            image: symbolImage(name: "icon_gnb_sleep"),
-            selectedImage: nil
-        )
+        let sleepViewController = placeholderTab(title: "수면", imageName: "icon_gnb_sleep")
 
         let morningViewController = tabBarNavigationController(
-            unselectedImage: symbolImage(name: "icon_gnb_morningoff"),
-            selectedImage: symbolImage(name: "icon_gnb_morningon"),
+            unselectedImage: assetImage(name: "icon_gnb_morningoff"),
+            selectedImage: assetImage(name: "icon_gnb_morningon"),
             rootViewController: MorningViewController(),
             title: "아침"
         )
 
-        let reportViewController = UIViewController()
-        reportViewController.tabBarItem = UITabBarItem(
-            title: "리포트",
-            image: symbolImage(name: "icon_gnb_report"),
-            selectedImage: nil
-        )
+        let reportViewController = placeholderTab(title: "리포트", imageName: "icon_gnb_report")
 
-        let settingViewController = UIViewController()
-        settingViewController.tabBarItem = UITabBarItem(
-            title: "설정",
-            image: symbolImage(name: "person.circle"),
-            selectedImage: nil
-        )
+        let settingViewController = placeholderTab(title: "설정", imageName: "icon_gnb_setting")
+
 
         viewControllers = [alarmViewController, sleepViewController, morningViewController, reportViewController, settingViewController]
         tabBar.tintColor = .white
     }
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        guard let index = viewControllers?.firstIndex(of: viewController),
+              let tabIndex = TabIndex(rawValue: index) else { return true }
 
-    func setupTabBarAppearance() {
+        let disabledTabs: [TabIndex] = [.sleep, .report, .setting]
+        return !disabledTabs.contains(tabIndex)
+    }
+
+    private func setupTabBarAppearance() {
         let appearance = UITabBarAppearance()
         appearance.configureWithOpaqueBackground()
-        
-        // TODO: - 아래 주석 코드로 변경 예정
-        appearance.backgroundColor = UIColor.gray
-//        appearance.backgroundColor = UIColor.appColor(.grey900)
+        appearance.backgroundColor = UIColor.gray // TODO: 앱 색상으로 변경 예정
 
         let itemAppearance = UITabBarItemAppearance()
-        
-        // TODO: - Pretendard로 변경 예정
-        let font = UIFont.systemFont(ofSize: 10, weight: .regular)
-
         appearance.stackedLayoutAppearance = itemAppearance
+
         tabBar.standardAppearance = appearance
         tabBar.scrollEdgeAppearance = appearance
     }
 
-    func tabBarNavigationController(unselectedImage: UIImage, selectedImage: UIImage, rootViewController: UIViewController, title: String) -> UINavigationController {
+    private func tabBarNavigationController(
+        unselectedImage: UIImage,
+        selectedImage: UIImage,
+        rootViewController: UIViewController,
+        title: String
+    ) -> UINavigationController {
         let navigation = UINavigationController(rootViewController: rootViewController)
         navigation.tabBarItem = UITabBarItem(title: title, image: unselectedImage, selectedImage: selectedImage)
         return navigation
     }
     
-    func symbolImage(name: String) -> UIImage {
-        return UIImage(systemName: name)?.withRenderingMode(.alwaysTemplate) ?? UIImage()
+    private func placeholderTab(title: String, imageName: String) -> UIViewController {
+        let viewController = UIViewController()
+        viewController.tabBarItem = UITabBarItem(
+            title: title,
+            image: assetImage(name: imageName),
+            selectedImage: nil
+        )
+        return viewController
     }
     
-    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-        guard let index = viewControllers?.firstIndex(of: viewController) else { return true }
-
-        let disabledTabs = [1, 3, 4]
-
-        return !disabledTabs.contains(index)
+    private func assetImage(name: String) -> UIImage {
+        return UIImage(named: name)?.withRenderingMode(.alwaysTemplate) ?? UIImage()
     }
 }
