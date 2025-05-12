96a622466b0615cb77f51e111edbb15be3696783
eunji <oieunxe@gmail.com>
2025-05-12 23:03:54 +0900

    style: 하단 탭 바 구현

---
+++ Alarmy-iOS/Alarmy-iOS/Presentation/AlarmyabBarController.swift
@@ -0,0 +1,96 @@
+//
+//  AlarmyTabBarController.swift
+//  Alarmy-iOS
+//
+//  Created by 이은지 on 5/12/25.
+//
+
+import UIKit
+
+class AlarmyTabBarController: UITabBarController, UITabBarControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        delegate = self
+        
+        configureController()
+        setupTabBarAppearance()
+    }
+
+    func configureController() {
+        let alarmViewController = tabBarNavigationController(
+            unselectedImage: symbolImage(name: "iconGnbClockoff"),
+            selectedImage: symbolImage(name: "iconGnbClockon"),
+            rootViewController: AlarmViewController(),
+            title: "알람"
+        )
+
+        let sleepViewController = UIViewController()
+        sleepViewController.tabBarItem = UITabBarItem(
+            title: "수면",
+            image: symbolImage(name: "iconGnbSleep"),
+            selectedImage: nil
+        )
+
+        let morningViewController = tabBarNavigationController(
+            unselectedImage: symbolImage(name: "iconGnbMorningoff"),
+            selectedImage: symbolImage(name: "iconGnbMorningon"),
+            rootViewController: MorningViewController(),
+            title: "아침"
+        )
+
+        let reportViewController = UIViewController()
+        reportViewController.tabBarItem = UITabBarItem(
+            title: "리포트",
+            image: symbolImage(name: "person.circle"),
+            selectedImage: nil
+        )
+
+        let settingViewController = UIViewController()
+        settingViewController.tabBarItem = UITabBarItem(
+            title: "설정",
+            image: symbolImage(name: "person.circle"),
+            selectedImage: nil
+        )
+
+        viewControllers = [alarmViewController, sleepViewController, morningViewController, reportViewController, settingViewController]
+        tabBar.tintColor = .white
+    }
+
+    func setupTabBarAppearance() {
+        let appearance = UITabBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        
+        // TODO: - 아래 주석 코드로 변경 예정
+        appearance.backgroundColor = UIColor.gray
+//        appearance.backgroundColor = UIColor.appColor(.grey900)
+
+        let itemAppearance = UITabBarItemAppearance()
+        
+        // TODO: - Pretendard로 변경 예정
+        let font = UIFont.systemFont(ofSize: 10, weight: .regular)
+
+        appearance.stackedLayoutAppearance = itemAppearance
+        tabBar.standardAppearance = appearance
+        tabBar.scrollEdgeAppearance = appearance
+    }
+
+    func tabBarNavigationController(unselectedImage: UIImage, selectedImage: UIImage, rootViewController: UIViewController, title: String) -> UINavigationController {
+        let navigation = UINavigationController(rootViewController: rootViewController)
+        navigation.tabBarItem = UITabBarItem(title: title, image: unselectedImage, selectedImage: selectedImage)
+        return navigation
+    }
+    
+    func symbolImage(name: String) -> UIImage {
+        return UIImage(systemName: name)?.withRenderingMode(.alwaysTemplate) ?? UIImage()
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        guard let index = viewControllers?.firstIndex(of: viewController) else { return true }
+
+        let disabledTabs = [1, 3, 4]
+
+        return !disabledTabs.contains(index)
+    }
+
+}
---
+++ Alarmy-iOS/Alarmy-iOS/Presentation/SubViews/AlarmViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmViewController.swift
+//  Alarmy-iOS
+//
+//  Created by 이은지 on 5/12/25.
+//
+
+import UIKit
+
+class AlarmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
