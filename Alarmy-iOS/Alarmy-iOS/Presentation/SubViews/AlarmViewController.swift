24d4dd6cbc46d94f096865bdec0d15251ba00975
eunji <oieunxe@gmail.com>
2025-05-14 23:08:05 +0900

    style: 다음 알람 버튼 추가

--- Alarmy-iOS/Alarmy-iOS/Presentation/SubViews/AlarmViewController.swift
+++ Alarmy-iOS/Alarmy-iOS/Presentation/SubViews/AlarmViewController.swift
@@ -7,11 +7,104 @@
 
 import UIKit
 
-class AlarmViewController: UIViewController {
+import SnapKit
+import Then
 
+final class AlarmViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - UI Properties
+    private let homeHeaderButton = UIButton()
+    private let nextAlarmButton = UIButton()
+    private let timeAlarmLabel = UILabel()
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        
+        setHierarchy()
+        setStyle()
+        setLayout()
+        
     }
     
+    
+    // MARK: - UI Function
+    private func setHierarchy() {
+        view.addSubViews(
+            homeHeaderButton,
+            nextAlarmButton,
+            timeAlarmLabel
+        )
+    }
+    
+    private func setStyle() {
+        homeHeaderButton.do {
+            $0.setImage(UIImage(named: "icon_header_menu"), for: .normal)
+        }
+        
+        nextAlarmButton.do {
+            var config = UIButton.Configuration.filled()
+            let title = "다음 알람"
+
+            let attributedTitle = AttributedString(title, attributes: AttributeContainer([
+                .font: UIFont.body4,
+                .foregroundColor: UIColor.white
+            ]))
+
+            config.attributedTitle = attributedTitle
+            config.image = UIImage(named: "icon_home_arrow")
+            config.baseBackgroundColor = UIColor.appColor(.grey900)
+            config.imagePlacement = .trailing
+            config.imagePadding = 4
+            config.cornerStyle = .capsule
+
+            $0.configuration = config
+        }
+        
+        timeAlarmLabel.do {
+            $0.text = "9시간 22분 후에 울려요"
+            $0.textColor = UIColor.appColor(.grey100)
+            $0.font = .title5
+        }
+
+    }
+    
+    private func setLayout() {
+        view.backgroundColor = UIColor.appColor(.grey950)
+        
+        homeHeaderButton.snp.makeConstraints {
+            $0.top.equalTo(view.snp.top).offset(70)
+            $0.trailing.equalToSuperview().offset(-16)
+            $0.width.height.equalTo(32)
+        }
+        
+        nextAlarmButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().offset(16)
+            $0.top.equalTo(homeHeaderButton.snp.bottom).offset(16)
+            $0.width.greaterThanOrEqualTo(76)
+            $0.height.equalTo(30)
+        }
+        
+        timeAlarmLabel.snp.makeConstraints {
+            $0.leading.equalTo(nextAlarmButton.snp.leading).offset(8)
+            $0.top.equalTo(nextAlarmButton.snp.bottom).offset(16)
+        }
+    }
+    
+    // MARK: - Function
+    private func bind() {
+        
+    }
+    
+    private func setDelegate() {
+        // delegate, datasource 모음
+    }
+    
+    private func setAddTarget() {
+        // addtarget 모음
+    }
+    
+    // MARK: - objc
 }
