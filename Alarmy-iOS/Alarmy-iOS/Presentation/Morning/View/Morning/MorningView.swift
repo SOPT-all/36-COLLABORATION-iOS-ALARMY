3097a4c6659a89434e1c4d8bc8b54943d8c2bab5
JURI <80569323+juri123123@users.noreply.github.com>
2025-05-15 14:04:41 +0900

    style: #13 아침 UI

---
+++ Alarmy-iOS/Alarmy-iOS/Presentation/Morning/View/Morning/MorningView.swift
@@ -0,0 +1,93 @@
+//
+//  MorningView.swift
+//  Alarmy-iOS
+//
+//  Created by 최주리 on 5/14/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class MorningView: UIView {
+    
+    // MARK: - Properties
+    private let backgroundImageView = UIImageView()
+    
+    private let informationView = HeaderInformationView()
+    
+    private let contentStackView = UIStackView()
+    private let todaySentenceView = TodaySentenceView()
+    private let morningMoodView = MorningMoodView()
+    private let stellaView = StellaView()
+     
+    // MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setStyle()
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+//MARK: - ViewConfigurable protocol
+extension MorningView: ViewConfigurable {
+    func setStyle() {
+        backgroundImageView.do {
+            $0.image = .morningBackground
+        }
+        
+        contentStackView.do {
+            $0.axis = .vertical
+            $0.spacing = 16
+        }
+    }
+    
+    func setHierarchy() {
+        self.addSubViews(
+            backgroundImageView,
+            informationView,
+            contentStackView
+        )
+        
+        contentStackView.addArrangedSubViews(
+            todaySentenceView,
+            morningMoodView,
+            stellaView
+        )
+    }
+    
+    func setLayout() {
+        backgroundImageView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        
+        informationView.snp.makeConstraints {
+            $0.top.leading.trailing.equalToSuperview().inset(60)
+            $0.height.equalTo(202)
+        }
+        
+        contentStackView.snp.makeConstraints {
+            $0.top.equalTo(informationView.snp.bottom).offset(32)
+            $0.horizontalEdges.equalToSuperview().inset(15)
+        }
+        
+        todaySentenceView.snp.makeConstraints {
+            $0.height.equalTo(208)
+        }
+        
+        morningMoodView.snp.makeConstraints {
+            $0.height.equalTo(72)
+        }
+        
+        stellaView.snp.makeConstraints {
+            $0.height.equalTo(104)
+        }
+    }
+}
---
+++ Alarmy-iOS/Alarmy-iOS/Presentation/Morning/View/Morning/TodaySentenceView.swift
@@ -0,0 +1,81 @@
+//
+//  TodaySentenceView.swift
+//  Alarmy-iOS
+//
+//  Created by 최주리 on 5/14/25.
+//
+
+import UIKit
+
+final class TodaySentenceView: UIView {
+    
+    // MARK: - Properties
+    private let todayLabel = UILabel()
+    private let shareButton = UIButton()
+    private let todayImageView = UIImageView()
+
+    //MARK: - Initializer
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setStyle()
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - ViewConfigurable protocol
+extension TodaySentenceView: ViewConfigurable {
+    func setStyle() {
+        self.makeBorder(width: 0, color: .clear, cornerRadius: 15)
+        self.backgroundColor = UIColor.appColor(.greyOpacity900)
+        
+        todayLabel.do {
+            $0.text = "오늘의 문장"
+            $0.font = .body1
+            $0.textColor = .white
+        }
+        var configuration = UIButton.Configuration.borderedTinted()
+        configuration.title = "공유하기"
+        configuration.titleAlignment = .center
+        configuration.attributedTitle?.font = UIFont.body3
+        configuration.baseForegroundColor = .white
+        
+        configuration.baseBackgroundColor = UIColor.clear
+
+        
+        shareButton.do {
+            $0.configuration = configuration
+            $0.makeBorder(width: 1, color: .white, cornerRadius: 5)
+        }
+        
+        todayImageView.do {
+            $0.makeBorder(width: 0, color: .clear, cornerRadius: 16)
+        }
+    }
+    
+    func setHierarchy() {
+        self.addSubViews(todayLabel, shareButton, todayImageView)
+    }
+    
+    func setLayout() {
+        todayLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(19)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        shareButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(17)
+            $0.trailing.equalToSuperview().inset(21)
+        }
+        
+        todayImageView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(60)
+            $0.horizontalEdges.equalToSuperview().inset(20)
+        }
+    }
+}
