0ab9e9af9ea1b298a3b9fbb944e282293d51e785
eunji <oieunxe@gmail.com>
2025-05-22 23:25:13 +0900

    feat: 알람 API 요청 Service 구현

--- Alarmy-iOS/Alarmy-iOS/Network/Service/AlarmService.swift
+++ Alarmy-iOS/Alarmy-iOS/Network/Service/AlarmService.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+protocol AlarmProtocol {
+    func fetchAlarmList() async throws -> [AlarmEntity]
+}
+
+struct DefaultAlarmService: AlarmProtocol {
+    private let network = BaseService.shared
+    
+    func fetchAlarmList() async throws -> [AlarmEntity] {
+        do {
+            let response: [AlarmResponse] = try await network.request(endPoint: .alarms)
+            return response.map { $0.toEntity() }
+        } catch {
+            print("통신 실패: \(error)")
+            return [.mock()]
+        }
+    }
+}
