7e6013d8b330827b6a18176a81ffbabb6ac6770d
eunji <oieunxe@gmail.com>
2025-05-23 01:11:04 +0900

    feat: 알람 체크 서비스 구현

---
+++ Alarmy-iOS/Alarmy-iOS/Network/Service/AlarmCheckService.swift
@@ -0,0 +1,37 @@
+//
+//  AlarmCheckService.swift
+//  Alarmy-iOS
+//
+//  Created by 이은지 on 5/23/25.
+//
+
+import Foundation
+
+protocol AlarmCheckServiceProtocol {
+    func fetchAlarmCheck(currentTime: String) async throws -> String
+}
+
+final class DefaultAlarmCheckService: AlarmCheckServiceProtocol {
+    private let network = BaseService.shared
+    
+    func fetchAlarmCheck(currentTime: String) async throws -> String {
+        do {
+            let request = AlarmCheckRequest(currentTime: currentTime)
+            let response: AlarmCheckResponse = try await network.request(
+                endPoint: .alarmCheck,
+                body: request
+            )
+
+            let shouldTriggerID = response.alarmInfo
+                .filter { $0.shouldTrigger }
+                .map { String($0.id) }
+                .joined(separator: ", ")
+
+            return shouldTriggerID
+        } catch {
+            print("get 실패 ~")
+            return ""
+        }
+    }
+
+}
