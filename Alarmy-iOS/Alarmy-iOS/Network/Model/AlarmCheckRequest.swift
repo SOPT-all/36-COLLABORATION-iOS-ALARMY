95ee7858a4e5dd5fd81c09618e06d6c315900342
eunji <oieunxe@gmail.com>
2025-05-23 01:12:37 +0900

    feat: 알람 체크 Request 및 Response 구현

---
+++ Alarmy-iOS/Alarmy-iOS/Network/Model/AlarmCheckRequest.swift
@@ -0,0 +1,12 @@
+//
+//  AlarmCheckRequest.swift
+//  Alarmy-iOS
+//
+//  Created by 이은지 on 5/23/25.
+//
+
+import Foundation
+
+struct AlarmCheckRequest: Encodable {
+    let currentTime: String
+}
