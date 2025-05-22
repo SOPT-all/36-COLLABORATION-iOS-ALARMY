4de4eedea1d9e28e005da9c20d3953dadd5339ce
eunji <oieunxe@gmail.com>
2025-05-22 17:30:42 +0900

    refactor: 파일 구조 분리

---
+++ Alarmy-iOS/Alarmy-iOS/Network/Service/AlarmService.swift
@@ -0,0 +1,8 @@
+//
+//  AlarmService.swift
+//  Alarmy-iOS
+//
+//  Created by 이은지 on 5/22/25.
+//
+
+import Foundation
---
+++ Alarmy-iOS/Alarmy-iOS/Presentation/Alarm/Model/AlarmEntity.swift
@@ -0,0 +1,8 @@
+//
+//  AlarmEntity.swift
+//  Alarmy-iOS
+//
+//  Created by 이은지 on 5/22/25.
+//
+
+import Foundation
